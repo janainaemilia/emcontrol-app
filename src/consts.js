1105b7fc7597eafc385de347d39462bbe20761b2
Janaina Emilia <janaina.abc3@gmail.com>
2019-10-28 14:10:22 -0300

    Set production api url

--- src/consts.js
+++ src/consts.js
@@ -1,4 +1,4 @@
 export default {
     //API_URL: 'http://localhost:8080/api
-    API_URL: 'http://emcontrol-api.us-east-2.elasticbeanstalk.com'
+    API_URL: 'http://api-emcontrol.us-east-2.elasticbeanstalk.com'
 }
